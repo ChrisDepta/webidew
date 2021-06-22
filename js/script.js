05570305a93a4eddafd2f2ea5437b61719198fc6
Chris Depta <christophdepta@gmail.com>
2021-06-22 17:26:22 +0200

    animation for menu nav baner and orientation landscape for mobile.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
 // Burger function for menu and burger animation
-document.querySelector('.burger').addEventListener('click', function(){
+const burger = document.querySelector('.burger');
+const menuAnimation = function(){
     this.classList.toggle('active');
     document.querySelector('.wrap').classList.toggle('active');
     document.querySelector('.nav').classList.toggle('active');
-    document.querySelector('.menu').classList.toggle('active');
-}) 
-// MENU Links mouseOver and mouseleave function with underline from left to right out
-const links = document.querySelectorAll('.menu__list a');
-console.log(links);
+    document.querySelector('.menu').classList.toggle('active'); 
+    sentenceAnimation();
+};
 
-links.forEach(link => link.addEventListener('mouseover', function(){
-    let span = this.lastChild;
-    console.log(span)
-    span.classList.add('active'); 
-}));
-links.forEach(link => link.addEventListener('mouseleave', function(){
-    let span = this.lastChild;
-    span.classList.remove('active');
-    span.classList.add('removeActive');
-    setTimeout( ()=> span.classList.remove('removeActive'), 1000);
-    }))
+const sentenceAnimation = function(){
+    let words = document.querySelectorAll('.baner__sentence__words div');
+    const about = document.querySelector('.baner__sentence__about span');
+    about.classList.toggle('active');
+    words.forEach(word => {word.classList.toggle('active')});
+}
+const startSentenceAnimation = ()=> {
+    setTimeout(sentenceAnimation, 2000);
+}
+window.addEventListener('load', startSentenceAnimation);
+burger.addEventListener('click', menuAnimation);
