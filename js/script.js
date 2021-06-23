414102d8ea7c8f236de09345deeb714b130e5015
Chris Depta <christophdepta@gmail.com>
2021-06-23 16:26:19 +0200

    @media queries for more elements

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 // Burger function for menu and burger animation
 const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('.nav');
+const wrap = document.querySelector('.wrap');
+
 const menuAnimation = function(){
-    this.classList.toggle('active');
-    document.querySelector('.wrap').classList.toggle('active');
-    document.querySelector('.nav').classList.toggle('active');
-    document.querySelector('.menu').classList.toggle('active'); 
+    burger.classList.toggle('active');
+    wrap.classList.toggle('active');
+    nav.classList.toggle('active');
+    menu.classList.toggle('active'); 
     sentenceAnimation();
+    if(menu.classList.contains('active')){
+        nav.style.backgroundColor = 'transparent';
+    } else {
+        nav.style.backgroundColor = '#f4f5f6';
+    }
 };
+const menuAnimationEnd = function(){
+    if(this.classList.contains('active')){
+    burger.classList.remove('active');
+    wrap.classList.remove('active');
+    nav.classList.remove('active');
+    menu.classList.remove('active'); 
+    sentenceAnimation();
+    nav.style.backgroundColor = '#f4f5f6';
+    } else return
+}
 // Baner sentence animation for window ready and for
 // event click menu with wrap replace
 const sentenceAnimation = function(){
@@ -23,9 +42,10 @@ burger.addEventListener('click', menuAnimation);
 
 // Nav add color on background for scroll event
 window.addEventListener('scroll', function (){
-    document.querySelector('.nav').style.backgroundColor = "#f4f5f6";
-    if(this.scrollY === 0) {
-    document.querySelector('.nav').style.backgroundColor = "transparent";
+    if(menu.classList.contains('active') || this.scrollY === 0) {
+        nav.style.backgroundColor = "transparent";
+        return
     }
-    console.log(this.scrollY);
+    nav.style.backgroundColor = "#f4f5f6";
 });
+wrap.addEventListener('click', menuAnimationEnd);
